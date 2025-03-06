8eda6e509b203fe2673070ecc3907c4ba9773a34
Mista299 <michael.tabares@udea.edu.co>
2025-03-06 11:50:20 -0500

    rutas bug

--- routes/deportistaRoutes.js
+++ routes/deportistaRoutes.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.post('/', crearDeportista, verificarToken);
 //editar como admin
 router.get('/', verificarToken, verificarAdmin, obtenerDeportistas);
-router.put('/:cedula_deportista', verificarToken, verificarAdmin, editarDeportista );
+router.put('/:_id', verificarToken, verificarAdmin, editarDeportista );
 //editar como usuario
 router.put('/editar', verificarToken, editarDeportistaFromUser );
 
