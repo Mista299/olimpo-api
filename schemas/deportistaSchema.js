30a18c5704bd64b586d3fd1b6b6eeef263876042
Mista299 <michael.tabares@udea.edu.co>
2025-03-17 21:07:48 -0500

    email_deportista bug in zod

--- schemas/deportistaSchema.js
+++ schemas/deportistaSchema.js
@@ -3,7 +3,6 @@ import { z } from 'zod';
 export const DeportistaSchema = z.object({
   cedula_deportista: z.string().min(1, { message: "La cédula es obligatoria" }),
   nombre_deportista: z.string().min(1, { message: "El nombre es obligatorio" }),
-  email_deportista: z.string().email({ message: "El correo no es válido" }).optional(),
   direccion_deportista: z.string().min(1, { message: "La dirección es obligatoria" }),
   telefono_deportista: z.string().min(1, { message: "El teléfono es obligatorio" }),
   eps_deportista: z.string().min(3, { message: "La EPS es obligatoria" }),
